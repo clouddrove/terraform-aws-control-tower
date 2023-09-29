382f3e81abda5d68760f9de30c51c03e568d1f10
Anmol Nagpal <ianmolnagpal@gmail.com>
2023-09-29 12:29:38 -0400

    update

--- _examples/single-account/main.tf
+++ _examples/single-account/main.tf
@@ -3,19 +3,30 @@ locals {
   environment = "networking"
   region      = "us-east-1"
   domain      = "identos.ca"
+  role_arn    = "arn:aws:iam::123456789012:role/role-name"
+  cidr_block  = "10.0.10.0/16"
+  subnet_type = private
+
 }
 
 provider "aws" {
   region = local.region
 }
 
+provider "aws" {
+  alias = "networking"
+  assume_role {
+    role_arn = local.role_arn
+  }
+  region = local.region
+}
 module "CT" {
   source      = "../../"
   name        = local.name
   environment = local.environment
 
-  cidr_block  = "10.0.10.0/16"
-  subnet_type = private
+  cidr_block  = local.cidr_block
+  subnet_type = local.subnet_type
 
   ## ACM
   domain_name = local.domain
