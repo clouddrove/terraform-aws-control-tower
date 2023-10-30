d36aea915a1aa5ec50c6661df69683daf1936452
vishwajitnagulkar <nagulkarvishwajit1999@gmail.com>
2023-10-30 21:25:11 +0530

    fix: fixed tfsec issues

--- _module/oidc/main.tf
+++ _module/oidc/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  account_id = data.aws_caller_identity.current.account_id
+#   account_id = data.aws_caller_identity.current.account_id
   tags = {
     "name"        = var.name
     "repository"  = var.repository
