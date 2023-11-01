55719d8a500e4822a764e62e1b464d469bf947e1
Himanshu Ahirwar <himanshu.ahirwar@clouddrove.com>
2023-11-01 23:04:31 +0530

    fix - tfchecks

--- _examples/multi-account/networking/outputs.tf
+++ _examples/multi-account/networking/outputs.tf
@@ -2,12 +2,12 @@
 # Outputs
 # ------------------------------------------------------------------------------
 output "transit_gateway_id" {
-  value       = module.CT.transit_gateway_id
+  value       = module.CT.tgw_hub_transit_gateway_id
   description = "The ID of the Transit Gateway."
 }
 
 output "resource_share_arn" {
-  value       = module.CT.resource_share_arn
+  value       = module.CT.tgw_hub_resource_share_arn
   description = "The ARN  of the RAM."
 }
 
