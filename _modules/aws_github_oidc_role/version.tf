bdfc46c253e8294fef260f0fa4acc47c62149720
anmolnagpal <anmol@clouddrove.com>
2024-01-08 19:04:45 +0530

    feat: update tf.checks step name

--- _modules/aws_github_oidc_role/version.tf
+++ _modules/aws_github_oidc_role/version.tf
@@ -2,15 +2,15 @@
 # Versions
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.6.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+      version = ">= 5.31.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.0"
+      version = ">= 4.0.5"
     }
   }
 }
